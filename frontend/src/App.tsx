5b5b69f53e5822d979402161bd13ac8920090341
vishalsharma <vishalssharma811@gmail.com>
2024-04-30 17:59:58 +0530

    Started again

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,10 @@
-import { BrowserRouter , Routes , Route } from "react-router-dom";
-import Homepage from './pages/Homepage';
+
 function App() {
   
   return (
-    <BrowserRouter>
-       <Routes>
-           <Route path="/home" element={<Homepage/>}/>
-           
-       </Routes>
-    </BrowserRouter>
+   <>
+       <h1>movie zone</h1>
+   </>
   )
 }
 
