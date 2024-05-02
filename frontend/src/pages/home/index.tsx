c9b5b959044c4074b77a7f9840ed221a96871f9a
vishalsharma <vishalssharma811@gmail.com>
2024-05-02 23:28:26 +0530

    Created a SearchBar

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,14 +1,52 @@
-import Layout from "../../Layout";
+import { SetStateAction, useState } from "react";
+import { Box, InputBase, Paper , InputAdornment } from "@mui/material";
+import SearchIcon from '../../assets/icons/Searchicon.svg'
+import Layout from "../../Layout"
+
+
 
 const Home=()=>{
+      const [search , setSearch] = useState("");
+      const handleSearch =(e:{target:{value:SetStateAction<string>}})=>{
+           setSearch(e.target.value);
+      }
     return(
         <>
-        <Layout>
-               <div>
-                   <h1>Home page</h1>
-                   <p>This is the basic homepage.</p>
-               </div>
-        </Layout>
+           <Layout>
+               <Box>
+                  <Paper  component="form"
+                     sx={{
+                           display: "flex",
+                          alignItems: "center",
+                          borderRadius: "default",
+                           p: 1,
+                          backgroundColor: "#10141f",
+                          border: "none",
+                   }}>
+                       <InputBase
+                          placeholder="Search for movies or TV series..."
+                          sx={{
+                            ml: 1,
+                            flex: 1,
+                            color: "white",
+                            border: "none",
+                          }}
+                          value={search}
+                          onChange={handleSearch}
+                          startAdornment={
+                            <InputAdornment position="start">
+                              <img
+                                src={SearchIcon}
+                                alt="search icon"
+                                width={20}
+                                height={20}
+                              />
+                            </InputAdornment>
+                          }
+                       />
+                   </Paper>
+               </Box>
+           </Layout>
         </>
     )
 }
